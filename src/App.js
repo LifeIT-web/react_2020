39ebb66b2ca731f3abde092c17849c3a4b59e61d
LifeIT-web <vonmen12@naver.com>
2020-12-07 21:30:11 +0900

    음식앱

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({fav}){
-  return <h1>I like { fav }</h1>;
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h2>I like { name }</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+const foodILike = [ /* foodILike 변수에 빈 배열 저장 */
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi&f=1',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'samgyeopsal',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.v0RiALj-qb0tA3TaocYnVAHaE7%26pid%3DApi&f=1',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EbPzuedGhvUxXy7ufrwwAAHaE8%26pid%3DApi&f=1',
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.0gqVfoqJFU9YR9zBh7R0kAHaGS%26pid%3DApi&f=1',
+    rating: 4.1,
+  },
+  {
+    id: 5,
+    name: 'kimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi&f=1',
+    rating: 3.9,
+  },
+]; 
 
 function App() {
   return (
   <div>
-    <h1>Hello</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="chukumi" />
+    {
+      foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))
+    }
   </div>
-  )
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default App;
